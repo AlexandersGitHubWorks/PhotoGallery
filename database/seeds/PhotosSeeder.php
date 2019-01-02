2440e47883a3cf589779229f48e41ce0e9c22b18
AlexBorovskiy <alexanderszzzzzz@gmail.com>
2019-01-02 16:03:41 +0200

    Added loading test photo for PhotoSeeder

--- database/seeds/PhotosSeeder.php
+++ database/seeds/PhotosSeeder.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 use App\Photo;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Facades\Storage;
 
 class PhotosSeeder extends Seeder
 {
@@ -18,6 +20,20 @@ class PhotosSeeder extends Seeder
                 'name' => 'Photo' . $i,
                 'description' => 'Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit'
             ]);
+        }
+
+        // Downloads images from dummy data
+        $this->storePhotoDummyData();
+    }
+
+    public function storePhotoDummyData()
+    {
+        $filesystem = new Filesystem();
+        $from = public_path('dummy-photo');
+        $images = $filesystem->allFiles($from);
+        foreach ($images as $image) {
+            $path = '/public/photos/' . $image->getFilename();
+            Storage::put($path, $filesystem->get($image->getPathname()));
         }
     }
 }
